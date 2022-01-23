e70a404ce3d03d3b54b4e9831e438b3769d49c0f
Chris Duong <chrisduong83@gmail.com>
2022-01-23 19:38:49 +0100

    set cpu

--- terraform/http-server/main.tf
+++ terraform/http-server/main.tf
@@ -6,13 +6,19 @@ resource "aws_cloudwatch_log_group" "http_server" {
 resource "aws_ecs_task_definition" "http_server" {
   family = "http_server"
 
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  cpu    = 256
+  memory = 512
+
   container_definitions = <<EOF
 [
   {
     "name": "http_server",
     "image": "chrisduong/http-server:${var.app_version}",
-    "cpu": 0,
-    "memory": 128,
     "portMappings": [
       {
         "containerPort": 8080
@@ -52,4 +58,29 @@ resource "aws_ecs_service" "http_server" {
     target_group_arn = var.target_group_arn
   }
 
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${terraform.workspace}-ecsTaskExecutionRole"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
